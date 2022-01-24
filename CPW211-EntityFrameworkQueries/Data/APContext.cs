d98428f74b2cf24b021d6c00ebf6285aef55ddc9
Joseph Ortiz <JoeProgrammer88@users.noreply.github.com>
2022-01-24 09:04:05 -0800

    Switched EF Core logging from Command to Query

--- CPW211-EntityFrameworkQueries/Data/APContext.cs
+++ CPW211-EntityFrameworkQueries/Data/APContext.cs
@@ -33,7 +33,7 @@ namespace CPW211_EntityFrameworkQueries
                 optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=AP");
             }
 
-            optionsBuilder.LogTo(message => Debug.WriteLine(message), new[] {DbLoggerCategory.Database.Command.Name});
+            optionsBuilder.LogTo(message => Debug.WriteLine(message), new[] {DbLoggerCategory.Query.Name});
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
